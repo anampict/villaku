f1c3433ecb9b956186658e1b342efa313d0db57f
anampict <anampict@gmail.com>
2025-05-24 17:00:06 +0700

    Menambahkan halaman DaftarVilla

--- src/views/DaftarVillaView.vue
+++ src/views/DaftarVillaView.vue
@@ -18,7 +18,7 @@
     <div class="max-w-6xl mx-auto">
       <h1 class="text-4xl font-bold mb-8 text-center animate-fade-in">Daftar Villa</h1>
 
-      <div v-if="loading" class="text-center text-gray-500">Memuat data...</div>
+      <div v-if="loading" class="text-center text-gray-500">Memuat data villa...</div>
       <div
         v-else
         class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 animate-fade-in-delayed"
